de59b58199cc3f9d4723a9ecad035385c44e7edb
B3-13\User <3058ms@naver.com>
2025-07-16 18:46:32 +0900

    [김민서] 회원가입, 로그인 페이지 기능 구현 #1 , #2

--- src/main/java/com/minse0/kbc/user/UserController.java
+++ src/main/java/com/minse0/kbc/user/UserController.java
@@ -1,5 +1,31 @@
 package com.minse0.kbc.user;
 
-public class UserController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpSession;
+
+@RequestMapping("/user")
+@Controller
+public class UserController {
+	
+	@GetMapping("/join-view")
+	public String joinInput() {
+		return "user/join";
+	}
+	
+	@GetMapping("/login-view")
+	public String loginInput() {
+		return "user/login";
+	}
+	
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+	
+		session.removeAttribute("userId");
+		session.removeAttribute("userNickname");
+		
+		return "redirect:/user/login-view";
+	}
 }
--- src/main/java/com/minse0/kbc/user/domain/User.java
+++ src/main/java/com/minse0/kbc/user/domain/User.java
@@ -1,5 +1,52 @@
 package com.minse0.kbc.user.domain;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "User")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(length = 32, nullable = false, unique = true)
+    private String loginId;
+
+    @Column(length = 64, nullable = false)
+    private String password;
+
+    @Column(length = 32, nullable = false)
+    private String name;
+
+    @Column(length = 32, nullable = false)
+    private String nickname;
+
+    @Column(length = 16, nullable = false)
+    private String preferTeam;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 }
